ef225dd08ae0c9af454def8ac898d45e029fa3e6
Tuburan <jason.i.tuburan@accenture.com>
2019-04-15 15:40:52 +0800

    updates to deploy to heroku3

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+
 //Install express server
 const express = require('express');
 const path = require('path');
@@ -5,11 +6,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/mean-stack'));
+app.use(express.static('./dist/mean-stack'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/mean-stack/index.html'));
+res.sendFile(path.join(__dirname,'/dist/mean-stack/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
